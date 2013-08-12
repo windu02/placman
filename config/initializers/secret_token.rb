6b8ba74c2cf861dc12f3610fc9cd6d6c86504ddf
Christian Brel <ch.brel@gmail.com>
2013-08-12 11:05:50 +0200

    Init project with empty rails project. Addded Bootstrap.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Placman::Application.config.secret_key_base = '6685be5d839bf391c92e902f31b24b34474534c86b8de3576f11b32872689c7e6d4478d4736118e1431f37d183157e8f07d948f13068e5f8cb20aea58bafa93f'
