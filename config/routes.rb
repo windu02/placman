04c54c2c8f7988cf6ab9aab2042ae39d59c5968e
Christian Brel <ch.brel@gmail.com>
2013-08-17 01:25:16 +0200

    Added first page and updated locales with french for rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Placman::Application.routes.draw do
+  get "home/index"
+  
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
